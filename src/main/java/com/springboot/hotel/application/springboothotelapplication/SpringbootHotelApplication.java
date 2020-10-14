7e23ebf86a1d1577b2745df49b0404e8d9a51bf7
Felipe Guerreiro <f.guerreiro@live.com>
2020-10-14 17:42:51 -0300

    Update SpringbootHotelApplication.java
    Added modification to run guests page for testing

--- src/main/java/com/springboot/hotel/application/springboothotelapplication/SpringbootHotelApplication.java
+++ src/main/java/com/springboot/hotel/application/springboothotelapplication/SpringbootHotelApplication.java
@@ -2,6 +2,8 @@ package com.springboot.hotel.application.springboothotelapplication;
 
 import com.springboot.hotel.application.springboothotelapplication.data.entity.Room;
 import com.springboot.hotel.application.springboothotelapplication.data.repository.RoomRepository;
+import com.springboot.hotel.application.springboothotelapplication.data.entity.Guest;
+import com.springboot.hotel.application.springboothotelapplication.data.repository.GuestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,16 +21,30 @@ public class SpringbootHotelApplication {
 
 	@RestController
 	@RequestMapping("/rooms")
-	public class RoomController{
+	public class RoomController {
 		@Autowired
 		private RoomRepository roomRepository;
 
 		@GetMapping
-		public Iterable<Room> getRooms(){
+		public Iterable<Room> getRooms() {
 			return this.roomRepository.findAll();
 
 		}
 
 	}
 
-}
+		@RestController
+		@RequestMapping("/guests")
+		public class GuestController {
+			@Autowired
+			private GuestRepository guestRepository;
+
+			@GetMapping
+			public Iterable<Guest> getGuests() {
+				return this.guestRepository.findAll();
+
+			}
+
+		}
+
+	}
